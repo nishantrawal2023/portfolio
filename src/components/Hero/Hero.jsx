daf75599916b1edf6ea8c3227a3764a8b95d9bd3
nishantrawal2023 <nishantrawal.cs19@gmail.com>
2025-02-02 17:07:15 +0530

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Nishant Singh Rawal</h1>
+        <p className={styles.description}>
+        I'm a passionate Frontend Developer skilled in Reactjs, eager to build interactive and user-friendly web applications. Open to exciting opportunities!"
+        </p>
+        <a href="mailto:nishantrawal.cs19@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/profile.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
